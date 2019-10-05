1c7656a1ce19deabc9a222c1bbf6f36f741e43bc
soverly1 <46698309+soverly1@users.noreply.github.com>
2019-10-05 10:29:59 -0400

    Add files via upload

--- marvellookup.py
+++ marvellookup.py
@@ -8,45 +8,45 @@ import cgitb
 cgitb.enable()
 fileName="/home/class/SoftDev/marvel/marvel-wikia-data.csv"
 
-class CharacterEntry: 
-  def __init__(self, line : str):
-    line=line.strip()
-    data=line.split(",")
-    self.pageid=data[0]
-    self.char=data[1]
-    self.urlslug=data[2]
-    self.identify=data[3]
-    self.alignment=data[4]
-    self.eyecolor=data[5]
-    self.hair=data[6]
-    self.sex=data[7]
-    self.gsm=data[8]
-    self.alive=data[9]
-    self.appearances=data[10]
-    self.first_appear=data[11]
-    self.year=data[12]
+#class CharacterEntry: 
+#  def __init__(self, line : str):
+#    line=line.strip()
+#    data=line.split(",")
+#    self.pageid=data[0]
+#    self.char=data[1]
+#    self.urlslug=data[2]
+#    self.identify=data[3]
+#    self.alignment=data[4]
+#    self.eyecolor=data[5]
+#    self.hair=data[6]
+#    self.sex=data[7]
+#    self.gsm=data[8]
+#    self.alive=data[9]
+#    self.appearances=data[10]
+#    self.first_appear=data[11]
+#    self.year=data[12]
 
  
-class TheIndex: 
-  def __init__(self, fileName : str): 
-    char_names = open(fileName)
-    self.charmap={}
-    self.chars=[]
-    index=0
-    for line in char_names:  
-      charData=CharacterEntry(line) 
-      self.chars.append(charData) 
-      fullname=charData.char 
-      names=fullname.lower().split(" ") 
-      for name in names: 
-        if name in self.charmap:
-            self.charmap[name].append(index)
-        else: 
-            self.charmap[name]=[index] 
-      index+=1  
-    return
-  def getResults(self,user_entry): 
-    return self.charmap.get(user_entry) 
+#class TheIndex: 
+#  def __init__(self, fileName : str): 
+#    char_names = open(fileName)
+#    self.charmap={}
+#    self.chars=[]
+#    index=0
+#    for line in char_names:  
+#      charData=CharacterEntry(line) 
+#      self.chars.append(charData) 
+#      fullname=charData.char 
+#      names=fullname.lower().split(" ") 
+#      for name in names: 
+#        if name in self.charmap:
+#            self.charmap[name].append(index)
+#        else: 
+#            self.charmap[name]=[index] 
+#      index+=1  
+#    return
+#  def getResults(self,user_entry): 
+#    return self.charmap.get(user_entry) 
 
 
 def print_header():
@@ -54,18 +54,18 @@ def print_header():
 
 
 def main(): 
-  form = cgi.FieldStorage()
+  #form = cgi.FieldStorage()
   print_header()
-  full_charmap=TheIndex(fileName) 
-  user_entry=input()
-  print("Looking up matches for the name","'"+user_entry+"'"+". . .")
-  l=full_charmap.getResults(user_entry) 
-  result=""
-  for a in l:
-    print(a)
-    b=self.charmap.get(a)
-    result += b 
-  print(result)
-  return result
+  #full_charmap=TheIndex(fileName) 
+  #user_entry=input()
+  #print("Looking up matches for the name","'"+user_entry+"'"+". . .")
+  #l=full_charmap.getResults(user_entry) 
+  #result=""
+  #for a in l:
+    #print(a)
+    #b=self.charmap.get(a)
+    #result += b 
+  #print(result)
+  #return result
   
 main()
